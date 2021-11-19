87473fa996d0e4a5328a77e70c2cd034225241f3
HiggOps <2luznypl@gmail.com>
2021-11-19 21:51:02 +0100

    cleaning

--- ZSL/Programowanie/5_scripts/Programowanie 2021-09-30 - 5_form.php
+++ ZSL/Programowanie/5_scripts/Programowanie 2021-09-30 - 5_form.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h3>Figury geometryczne</h3>
-    <form action="./scripts/5_script.php" method="post">
+    <form action="./5_scripts/Programowanie 2021-09-30 - 5_script.php" method="post">
         <input type="text" name="name" placeholder="Podaj imię"><br><br>
         <input type="radio" name="geometricFigure" value="kwadrat" checked>Kwadrat<br><br>
         <input type="radio" name="geometricFigure" value="prostokat">Prostokąt<br><br>
