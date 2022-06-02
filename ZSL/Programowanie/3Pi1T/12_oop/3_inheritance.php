6cbffd8a82293ab9dd34fcf1f20abea42bd1be31
HiggOps <2luznypl@gmail.com>
2022-06-02 08:56:35 +0200

    s

--- ZSL/Programowanie/3Pi1T/12_oop/3_inheritance.php
+++ ZSL/Programowanie/3Pi1T/12_oop/3_inheritance.php
@@ -21,25 +21,37 @@ class Mammal extends Animal{
     {
         return $this->numberOfLegs;
     }
+    public function setNumberOfLegs(int $legs)
+    {
+        $this->numberOfLegs = $legs;
+    }
     
 }
 
 class Fish extends Animal{
     public $numberOfFins;
-    public function __construct($numberOfFins)
-    {
-        
-    }
+    
     public function getNumberOfFins()
     {
         return $this->numberOfFins;
+    }
+    public function setNumberOfFins(int $fins)
+    {
+        $this->numberOfFins = $fins;
     }
 }
 
 $pies1 = new Mammal("Carlos", "50kg");
 
 echo $pies1->getData();
-$pies1->numberOfLegs = 4;
+#$pies1->numberOfLegs = 4;
+$pies1->setNumberOfLegs(4);
 echo "Ilość łap: ".$pies1->getNumberOfLegs()."<br>";
 
+$ryba1 = new Fish("Oskar", "5kg");
+
+echo $ryba1->getData();
+
+$ryba1->setNumberOfFins(5);
+echo "Ilość płetw: ".$ryba1->getNumberOfFins()."<br>"
 ?>
